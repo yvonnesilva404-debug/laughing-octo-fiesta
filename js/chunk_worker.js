827dd34ceb52b7b3172c3f38ef0ec5b3d970558b
yvonnesilva404-debug <yvonne.silva.404@gmail.com>
2026-04-10 15:18:38 -0500

    Delete js directory

--- js/chunk_worker.js
+++
@@ -1,7 +0,0 @@
-self.onmessage = async ({ data: url }) => {
-    const res = await fetch(url);
-    const blob = await res.blob();
-    const ds = new DecompressionStream('gzip');
-    const text = await new Response(blob.stream().pipeThrough(ds)).blob().then(b => b.text());
-    self.postMessage(JSON.parse(text));
-};
